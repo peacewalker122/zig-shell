17554deb455ba43fa94510a6480f5265cad77afe
Daniel Anugerah <danielanugerah2004@gmail.com>
2024-12-22 20:46:57 +0800

    third

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,6 @@ const posix = std.posix;
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const allocator = std.heap.page_allocator;
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-
     // Dynamically allocate a slice of strings
     var paths = try allocator.alloc([]const u8, 3);
     defer allocator.free(paths);
@@ -21,10 +14,18 @@ pub fn main() !void {
     paths[1] = "/usr/bin";
     paths[2] = "/usr/local/bin";
 
-    const val = try pkg.check(allocator, user_input, paths);
-    if (!val) {
-        try stdout.print("{s}: command not found\n", .{user_input});
-        posix.exit(1);
+    while (true) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("$ ", .{});
+
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        const val = try pkg.check(allocator, user_input, paths);
+        if (!val) {
+            try stdout.print("{s}: command not found\n", .{user_input});
+        }
     }
 
     posix.exit(0);
